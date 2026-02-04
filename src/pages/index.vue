196b689a03c266412c6651565f9abbfc0a71fdae
Tim Lin <e0610k@gmail.com>
2026-02-04 15:41:43 +0800

    chore: remove useless comments

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,20 +2,10 @@
 defineOptions({
   name: 'IndexPage',
 })
-
-// const { data, error, isFetching } = useWeatherApi()
 </script>
 
 <template>
   <div>
-    <!-- <div v-if="isFetching">
-      Loading...
-    </div>
-
-    <div v-else-if="error">
-      Error: {{ error.message }}
-    </div> -->
-
     <WeatherCard />
   </div>
 </template>
